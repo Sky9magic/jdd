9fe3b7c412fa2de6d06d67971afcb8e794c0196c
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-04 15:26:41 +0700

    a

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,16 @@
 export default function header(){
     return(
         <header>
-            <h1>JD web</h1>
+            <h1>
+                Junior Dev Official Website
+            </h1>
+            <nav>
+                <ul><a href = "">Home</a></ul>
+                <ul><a href = "problems">Problems</a></ul>
+                <ul><a href = "submissions">Submissions</a></ul>
+                <ul><a href = "contests" >Contests</a></ul>
+                <ul><a href = "settings">Settings</a></ul>
+            </nav>
         </header>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
+import Header from './header'
+
 export default function Home() {
     return(
-        <header>
-            <h1>
-                jd web
-            </h1>
-            <nav>
-                <ul><a href = "">Home</a></ul>
-                <ul><a href = "problems">Problems</a></ul>
-                <ul><a href = "submissions">Submissions</a></ul>
-                <ul><a href = "contests" >Contests</a></ul>
-                <ul><a href = "settings">Settings</a></ul>
-            </nav>
-        </header>
+        <Header/>
     );
 }
 
