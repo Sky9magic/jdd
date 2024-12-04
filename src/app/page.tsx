0e53aa9cf4c34d15a72d0856e8c05f7cb3b3d81c
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-04 15:11:32 +0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
                 jd web
             </h1>
             <nav>
-                <ul><a href = "wow" >Home</a></ul>
-                <ul><a href = "wow" >Problems</a></ul>
-                <ul><a href = "wow" >Submissions</a></ul>
-                <ul><a href = "wow" >Contests</a></ul>
-                <ul><a href = "wow" >Settings</a></ul>
+                <ul><a href = "home">Home</a></ul>
+                <ul><a href = "problems">Problems</a></ul>
+                <ul><a href = "submissions">Submissions</a></ul>
+                <ul><a href = "contests" >Contests</a></ul>
+                <ul><a href = "settings">Settings</a></ul>
             </nav>
         </header>
     );
