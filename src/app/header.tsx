9674b3f30dbe10587f87f32814af6433bb9bb858
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-04 15:08:11 +0700

    a

---
+++ src/app/header.tsx
@@ -0,0 +1,7 @@
+export default function header(){
+    return(
+        <header>
+            <h1>JD web</h1>
+        </header>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import header from './header.tsx'
+
 export default function Home() {
     return(
-        <h1>
-            JD
-        </h1>
+        <header>
+            <h1>
+                jd web
+            </h1>
+            <nav>
+                <ul><a href = "wow" >Home</a></ul>
+                <ul><a href = "wow" >Problems</a></ul>
+                <ul><a href = "wow" >Submissions</a></ul>
+                <ul><a href = "wow" >Contests</a></ul>
+                <ul><a href = "wow" >Settings</a></ul>
+            </nav>
+        </header>
     );
 }
+
