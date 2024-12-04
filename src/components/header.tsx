e8548e1dd6a7bdfee05ad8132f1d24b3c5cc8b91
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-04 21:42:33 +0700

    YESYESYES

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import "../styles/header.css"
 
 export default function Header() {
     return (
-        <header>
-            <h1>Junior Dev Official Website</h1>
-            <nav>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/problems">Problems</Link></li>
-                    <li><Link href="/submissions">Submissions</Link></li>
-                    <li><Link href="/contests">Contests</Link></li>
-                    <li><Link href="/settings">Settings</Link></li>
-                </ul>
-            </nav>
+        <div className = "head">
+            <div className = "webname">
+                <img className = "logo" src = "/logo.png"></img>
+                <p style = {{margin: "15px", fontSize: "30px"}}>Junior Dev Official Website</p>
+            </div>
+            <div className = "headTextContent">
+                <div className = "headText">
+                    <p><Link href="/">Home</Link></p>
+                    <p><Link href="/problems">Problems</Link></p>
+                    <p><Link href="/submissions">Submissions</Link></p>
+                    <p><Link href="/contests">Contests</Link></p>
+                    <p><Link href="/settings">Settings</Link></p>
+                </div>
+            </div>
             <hr></hr>
-        </header>
+        </div>
     );
 }
