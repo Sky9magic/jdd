247d73b6944cb639ba0eca3189fe68a41d6a49bd
Skymagic <Sky9magic@gmail.com>
2024-12-05 10:32:32 +0700

    Update page.tsx

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -8,6 +8,14 @@ export default function Contests() {
             <h1>
                 Contests
             </h1>
+            <div className = "main">
+                <h1 style = {{fontSize: "50px", margin: "35px"}}>Contests</h2>
+                <center style = {{fontSize: "30px", margin: "35px"}}>Lorem ipsum dolor sit amet</center>
+                <h2 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>Upcoming Contests</h2>
+                <h3 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>contest</h3>
+                <h2 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>Past Contests</h2>
+                <list><h2 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>contest</h2></list>
+            </div>
         </>
     );
 }
