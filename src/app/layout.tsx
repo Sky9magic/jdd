add9697578aed84f747d605dbfe7483b7cd7050b
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-05 11:19:44 +0700

    adsf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     url: "https://beta-jdev.vercel.app/",
     images: [
       {
-        url: "/logo.jpg",
+        url: "../../public/logo.jpg",
         width: 1200,
         height: 630,
         alt: "Beta JD Logo",
