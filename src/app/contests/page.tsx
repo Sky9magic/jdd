c6d05656e1229284b1e444b505eef3d4e1bf30c1
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-05 10:52:59 +0700

    update contests

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Header from '../../components/header'
+import Header from '../../components/header';
+import "../../styles/contests.css"
 
 export default function Contests() {
     return(
@@ -9,12 +10,12 @@ export default function Contests() {
                 Contests
             </h1>
             <div className = "main">
-                <h1 style = {{fontSize: "50px", margin: "35px"}}>Contests</h2>
+                <h1 style = {{fontSize: "50px", margin: "35px"}}>Contests</h1>
                 <center style = {{fontSize: "30px", margin: "35px"}}>Lorem ipsum dolor sit amet</center>
                 <h2 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>Upcoming Contests</h2>
                 <h3 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>contest</h3>
                 <h2 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>Past Contests</h2>
-                <list><h2 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>contest</h2></list>
+                <h2 style = {{fontSize: "50px", margin: "35px",border: "1px"}}>contest</h2>
             </div>
         </>
     );
