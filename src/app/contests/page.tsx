3c5ab3bb62d4ff8683c16ada1cf4f8678ef182a2
Skymagic <Sky9magic@gmail.com>
2024-12-07 20:26:41 +0700

    Update page.tsx

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -33,9 +33,9 @@ export default function Contests() {
                             </center>
                             <div className="right-box">
                                 <div className = "list2">
-                                    <p><Link className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/560196"><b>Junior Dev Loy Krathong Contest 2024</b> <br></br> 16/Nov/2024 19:00 - 23:00</Link></p>
-                                    <p><Link className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/553124"><b>Pre-POSN1 DAY2 (2024)</b> <br></br> 9/Oct/2024 19:00 - 10/Oct/2024 00:00</Link></p>
-                                    <p><Link className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/553127"><b>Pre-POSN1 DAY1 (2024)</b> <br></br> 8/Oct/2024 19:00 - 9/Oct/2024 00:00</Link></p>
+                                    <a className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/560196"><b>Junior Dev Loy Krathong Contest 2024</b> <br></br> 16/Nov/2024 19:00 - 23:00</p>
+                                    <p className="contest"><Link className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/553124"><b>Pre-POSN1 DAY2 (2024)</b> <br></br> 9/Oct/2024 19:00 - 10/Oct/2024 00:00</Link></p>
+                                    <p className="contest"><Link className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/553127"><b>Pre-POSN1 DAY1 (2024)</b> <br></br> 8/Oct/2024 19:00 - 9/Oct/2024 00:00</Link></p>
                                 </div>
                             </div>
                         </div>
