8e20558000bd99fd6fc3dff39a70e01dca0a8dfb
Skymagic <Sky9magic@gmail.com>
2024-12-07 15:02:20 +0700

    Update page.tsx

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -12,7 +12,7 @@ export default function Contests() {
             <br></br>
             <br></br>
             <div className = "main">
-                <div className = "title"><center>Contests</center></div>
+                <div className = "title"><center>Contests (Time is displayed as UTC+7 timezone)</center></div>
                 <div className = "lines"><hr></hr></div>
                 <div className="cont">
                     <div className="container">
@@ -77,9 +77,9 @@ export default function Contests() {
                             </center>
                             <div className="right-box">
                                 <div className = "list2">
-                                    <p><Link className="btn" href = "https://codeforces.com/group/eScIVDG1u2/contest/560196">Junior Dev Loy Krathong Contest 2024</Link></p>
-                                    <p><Link className="btn" href = "https://codeforces.com/group/eScIVDG1u2/contest/553124">Pre-POSN1 DAY2 (2024)</Link></p>
-                                    <p><Link className="btn" href = "https://codeforces.com/group/eScIVDG1u2/contest/553127">Pre-POSN1 DAY1 (2024)</Link></p>
+                                    <Link className="btn" href = "https://codeforces.com/group/eScIVDG1u2/contest/560196"><h1>Junior Dev Loy Krathong Contest 2024</h1><br><p>16/Nov/2024 19:00 - 23:00</p></Link>
+                                    <Link className="btn" href = "https://codeforces.com/group/eScIVDG1u2/contest/553124"><h1>Pre-POSN1 DAY2 (2024)</h1><br><p>9/Oct/2024 19:00 - 10/Oct/2024 00:00</p></Link>
+                                    <Link className="btn" href = "https://codeforces.com/group/eScIVDG1u2/contest/553127"><h1>Pre-POSN1 DAY1 (2024)</h1><br><p>8/Oct/2024 19:00 - 9/Oct/2024 00:00</p></Link>
                                 </div>
                             </div>
                         </div>
