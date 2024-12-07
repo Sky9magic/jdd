6ab81fc72260887223ab414d0d8ed06cc4c9238a
Skymagic <Sky9magic@gmail.com>
2024-12-07 15:26:08 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     return (
         <>
             <Header/>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+            <br>
+            <br>
+            <br>
+            <br>
             <div className = "main">
                 <div className = "title"><h2>About</h2></div>
                 <center style = {{fontSize: "30px", margin: "30px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</center>
