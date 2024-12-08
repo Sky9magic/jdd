bc301635b4d63b14485cecb695d1468d91c1efa3
Skymagic <Sky9magic@gmail.com>
2024-12-08 18:14:21 +0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,11 +6,10 @@ export default function Header() {
     return (
         <div className = "head">
             <div className = "webname">
-                <Link href="/"><img className = "logo" src = "/logo.png"></img></Link>
-                <div className = "main"><p style = {{margin: "15px", fontSize: "30px", color: "black"}}><Link href="/">Junior Dev</Link></p></div>
+                <Link href="/"><img className = "logo" src = "/logo.png" alt="Junior Dev"></img></Link>
             </div>
-            <div className = "headTextContent">
-                <div className = "headText">
+            <div className="headTextContent">
+                <div className="headText">
                     <p><Link href="/">Home</Link></p>
                     <p><Link href="/problems">Problems</Link></p>
                     <p><Link href="/contests">Contests</Link></p>
