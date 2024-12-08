d9a896fad4998d19a1cff96690c3add515f4ebda
Skymagic <Sky9magic@gmail.com>
2024-12-08 18:12:41 +0700

    Update page.tsx

--- src/app/contests/page.tsx
+++ src/app/contests/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from '../../components/header';
 import "../../styles/contests.css"
-import Link from "next/link";
+{/*import Link from "next/link";*/}
 
 export default function Contests() {
     return(
@@ -22,7 +22,7 @@ export default function Contests() {
                             </center>
                             <div className="left-box">
                                 <div className = "list1">
-                                    <p>- contest</p>
+                                    <a className="contest" href = "#"><b>(insert contest here)</b>[16/Nov/2024 19:00 - 23:00]</a>
                                 </div>
                             </div>
                         </div>
@@ -33,9 +33,15 @@ export default function Contests() {
                             </center>
                             <div className="right-box">
                                 <div className = "list2">
-                                    <p style = {{margin: "15px"}}><Link className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/560196"><b>Junior Dev Loy Krathong Contest 2024</b> <br></br> 16/Nov/2024 19:00 - 23:00</Link></p>
-                                    <p style = {{margin: "15px"}}><Link className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/553124"><b>Pre-POSN1 DAY2 (2024)</b> <br></br> 9/Oct/2024 19:00 - 10/Oct/2024 00:00</Link></p>
-                                    <p style = {{margin: "15px"}}><Link className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/553127"><b>Pre-POSN1 DAY1 (2024)</b> <br></br> 8/Oct/2024 19:00 - 9/Oct/2024 00:00</Link></p>
+                                    <a className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/560196"><b>Junior Dev Loy Krathong Contest 2024</b>[16/Nov/2024 19:00 - 23:00]</a>
+                                    <br></br>
+                                    <br></br>
+                                    <a className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/553124"><b>Pre-POSN1 DAY2 (2024)</b>[9/Oct/2024 19:00 - 10/Oct/2024 00:00]</a>
+                                    <br></br>
+                                    <br></br>
+                                    <a className="contest" href = "https://codeforces.com/group/eScIVDG1u2/contest/553127"><b>Pre-POSN1 DAY1 (2024)</b>[8/Oct/2024 19:00 - 9/Oct/2024 00:00]</a>
+                                    <br></br>
+                                    <br></br>
                                 </div>
                             </div>
                         </div>
