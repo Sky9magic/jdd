9ba9bbb9cb204c692bdf57bc1a6bfbf2f19a1f28
opalInwza007x <114739286+opalInwza007x@users.noreply.github.com>
2024-12-12 20:38:36 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,8 @@ export default function Home() {
             <br></br>
             <div className = "main">
                 <div className = "title"><h2>About</h2></div>
-                <center style = {{fontSize: "30px", margin: "30px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. opal_Inwza007x was here help</center>
+                <center style = {{fontSize: "30px", margin: "30px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</center>
+                <p>opal_Inwza007x was here</p>
                 <p>Go to <Link className="btn-black" href="/contests">contests</Link></p>
             </div>
             <hr></hr>
