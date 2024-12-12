9545c7030076d7407737fb33786bae0350c1876c
opalInwza007x <114739286+opalInwza007x@users.noreply.github.com>
2024-12-12 23:37:41 +0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,1 +1,11 @@
+import React from "react";
+import Link from "next/link";
+import "../styles/footer_styles.css";
 
+export default function Header() {
+    return (
+        <footer class="footer">
+            <p>© 2024 Junior Dev. All rights reserved.</p>
+        </footer>  
+    );
+}
