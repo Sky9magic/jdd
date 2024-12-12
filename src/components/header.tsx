263477a7c7ebc4108acd4b04075086ba9ada7f5f
opalInwza007x <114739286+opalInwza007x@users.noreply.github.com>
2024-12-12 21:17:33 +0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,12 +5,10 @@ import "../styles/header_styles2.css"
 export default function Header() {
     return (
         <div className = "head">
-            <nav className="nav-head">
                 <Link href="/"><a>Home</a></Link>
                 <Link href="/problems"><a>Problem</a></Link>
                 <Link href="/contests"><a>Contest</a></Link>
                 <Link href="/settings"><a>Setting</a></Link>
-            </nav>
         </div>
         <div className = "head">
             <div className = "webname">
