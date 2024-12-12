1f74b824d89661b7261a810ae672439370301055
opalInwza007x <114739286+opalInwza007x@users.noreply.github.com>
2024-12-12 21:25:24 +0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,20 +4,37 @@ import "../styles/header_styles2.css"
 
 export default function Header() {
     return (
-        <div className = "head">
-            <div className = "webname">
-                <Link href="/"><img className = "logo" src = "/logo.png" alt="Junior Dev"></img></Link>
-                <Link href="/"><h1 style={{color:"black",marginLeft:"10px"}}>Junior Dev</h1></Link>
-            </div>
-            <div className="headTextContent">
-                <div className="headText">
-                    <p><Link href="/">Home</Link></p>
-                    <p><Link href="/problems">Problems</Link></p>
-                    <p><Link href="/contests">Contests</Link></p>
-                    <p><Link href="/settings">Settings</Link></p>
-                    <p><Link href="/">Help5</Link></p>
-                </div>
-            </div>
-        </div>
+        <header className="head">
+            <nav className="nav-head">
+                <Link href="/"><a>Home</a></Link>
+                <Link href="/problems"><a>Problem</a></Link>
+                <Link href="/contests"><a>Contest</a></Link>
+                <Link href="/settings"><a>Setting</a></Link>
+            </nav>
+        </header>
     );
 }
+
+// import React from "react";
+// import Link from "next/link";
+// import "../styles/header_styles2.css"
+
+// export default function Header() {
+//     return (
+//         <div className = "head">
+//             <div className = "webname">
+//                 <Link href="/"><img className = "logo" src = "/logo.png" alt="Junior Dev"></img></Link>
+//                 <Link href="/"><h1 style={{color:"black",marginLeft:"10px"}}>Junior Dev</h1></Link>
+//             </div>
+//             <div className="headTextContent">
+//                 <div className="headText">
+//                     <p><Link href="/">Home</Link></p>
+//                     <p><Link href="/problems">Problems</Link></p>
+//                     <p><Link href="/contests">Contests</Link></p>
+//                     <p><Link href="/settings">Settings</Link></p>
+//                     <p><Link href="/">Help5</Link></p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
