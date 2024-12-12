64d03747f24def115a5c5b4de0677c7f63b8902e
Skymagic <Sky9magic@gmail.com>
2024-12-12 21:19:56 +0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Link from "next/link";
-import "../styles/header.css"
+import "../styles/header_styles2.css"
 
 export default function Header() {
     return (
+        <div className = "head">
+            <nav className="nav-head">
+                <Link href="/"><a>Home</a></Link>
+                <Link href="/problems"><a>Problem</a></Link>
+                <Link href="/contests"><a>Contest</a></Link>
+                <Link href="/settings"><a>Setting</a></Link>
+            </nav>
+        </div>
         <div className = "head">
             <div className = "webname">
                 <Link href="/"><img className = "logo" src = "/logo.png" alt="Junior Dev"></img></Link>
@@ -13,11 +21,11 @@ export default function Header() {
                 <div className="headText">
                     <p><Link href="/">Home</Link></p>
                     <p><Link href="/problems">Problems</Link></p>
-                    <p><Link href="/contests">Competetions</Link></p>
+                    <p><Link href="/contests">Contests</Link></p>
                     <p><Link href="/settings">Settings</Link></p>
+                    <p><Link href="/">Help3</Link></p>
                 </div>
             </div>
-            <hr></hr>
         </div>
     );
 }
