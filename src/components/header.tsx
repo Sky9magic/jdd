65ecb7e9a62195c859d885e120e02612f6f20918
opalInwza007x <114739286+opalInwza007x@users.noreply.github.com>
2024-12-12 21:10:13 +0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,40 +1,24 @@
 import React from "react";
 import Link from "next/link";
-import "../styles/header_styles2.css"
+import "../styles/header.css"
 
 export default function Header() {
     return (
-        <header className="head">
-            <nav className="nav-head">
-                <a href="/">Home</a>
-                <a href="/problems">Problem</a>
-                <a href="/contests">Contest</a>
-                <a href="/settings">Setting</a>
-            </nav>
-        </header>
+        <div className = "head">
+            <div className = "webname">
+                <Link href="/"><img className = "logo" src = "/logo.png" alt="Junior Dev"></img></Link>
+                <Link href="/"><h1 style={{color:"black",marginLeft:"10px"}}>Junior Dev</h1></Link>
+            </div>
+            <div className="headTextContent">
+                <div className="headText">
+                    <p><Link href="/">Home</Link></p>
+                    <p><Link href="/problems">Problems</Link></p>
+                    <p><Link href="/contests">Contests</Link></p>
+                    <p><Link href="/settings">Settings</Link></p>
+                    <p><Link href="/">Help</Link></p>
+                </div>
+            </div>
+            <hr></hr>
+        </div>
     );
 }
-
-// import React from "react";
-// import Link from "next/link";
-// import "../styles/header.css"
-
-// export default function Header() {
-//     return (
-//         <div className = "head">
-//             <div className = "webname">
-//                 <Link href="/"><img className = "logo" src = "/logo.png" alt="Junior Dev"></img></Link>
-//                 <Link href="/"><h1 style={{color:"black",marginLeft:"10px"}}>Junior Dev</h1></Link>
-//             </div>
-//             <div className="headTextContent">
-//                 <div className="headText">
-//                     <p><Link href="/">Home</Link></p>
-//                     <p><Link href="/problems">Problems</Link></p>
-//                     <p><Link href="/contests">Contests</Link></p>
-//                     <p><Link href="/settings">Settings</Link></p>
-//                 </div>
-//             </div>
-//             <hr></hr>
-//         </div>
-//     );
-// }
