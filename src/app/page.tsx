e22014bef86733c32af39df56d4090616fcc332c
opalInwza007x <114739286+opalInwza007x@users.noreply.github.com>
2024-12-13 17:35:13 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
                 <div className="logo">
                     <img src = "https://beta-jdev.vercel.app/logo.png" alt="logo-JDev" />
                 </div>
-                <h1 className="logo_name">Junior Dev</h1>
-                <p>Junior Dev is Pretty SUS.<br />AMOGUS69 i dont know what to type xD.</p>
+                <h1 className="logo_name typing">Junior Dev</h1>
+                <p>opal_Inwza007x was here.<br />AMOGUS69 i dont know what to type xD.</p>
             </div>
             <div className="button-container">
                 <button className="custom-button">
