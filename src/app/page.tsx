39ff53a798bcfc8355fbdc384a3104b6a59cb665
opalInwza007x <114739286+opalInwza007x@users.noreply.github.com>
2024-12-13 17:56:09 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                     <img src = "https://beta-jdev.vercel.app/logo.png" alt="logo-JDev" />
                 </div>
                 <h1 className="logo_name typing">Junior Dev</h1>
-                <p>opal_Inwza007x was here.<br />That animation didn't bug; it is a feature.</p>
+                <p>opal_Inwza007x was here.<br />That animation did not bug; it is a feature.</p>
             </div>
             <div className="button-container">
                 <button className="custom-button">
