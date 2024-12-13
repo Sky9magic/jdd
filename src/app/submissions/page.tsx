5ae030ce350b8ea742df906396012967d5f6ddcc
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-13 19:05:29 +0700

    a

---
+++ src/app/submissions/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from '../../components/header'
+
+export default function Submissions() {
+    return(
+        <>
+            <Header/>
+            <h1>
+                Submissions
+            </h1>
+        </>
+    );
+}
