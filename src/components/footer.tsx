13a648179cc822d1452666dc3699992828d532a0
Skymagic <Sky9magic@gmail.com>
2024-12-14 12:36:21 +0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,6 +6,6 @@ export default function Footer() {
     return (
         <footer className="Footer">
             <center className="text-footer"><Link href="/">© 2024 Junior Dev. All rights reserved.</Link></center>
-        </footer>  
+        </footer>   
     );
 }
