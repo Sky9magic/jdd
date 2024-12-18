e008f88b20095aa711b7c8c2d7bff1774e5ec202
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-18 21:14:13 +0700

    problem page update

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -7,46 +7,46 @@ import Footer from "../../components/footer";
 
 export default function Problems() {
     const problemsList = [
-        { name: "Beginning", index: 0 },
-        { name: "CIA Robbery", index: 1 },
-        { name: "Taiyaki", index: 2 },
-        { name: "Dream", index: 3 },
-        { name: "Poisoning", index: 4 },
-        { name: "Eliminate", index: 5 },
-        { name: "Prime Move On", index: 6 },
-        { name: "Croissant", index: 7 },
-        { name: "Trap", index: 8 },
-        { name: "Rope Puzzle", index: 9 },
-        { name: "EXTRA", index: 10 },
-        { name: "Rocket", index: 11 },
-        { name: "Pond", index: 12 },
-        { name: "4 Khon", index: 13 },
-        { name: "DevJ and palindrome", index: 14 },
-        { name: "UOI", index: 15 },
-        { name: "Hurricane", index: 16 },
-        { name: "Sakubomb", index: 17 },
-        { name: "sleepntjoop", index: 18 },
-        { name: "Seating", index: 19 },
-        { name: "Friend", index: 20 },
-        { name: "EXTRA", index: 21 },
-        { name: "Original Krathong", index: 22 },
-        { name: "super memory", index: 23 },
-        { name: "Baidu X LOY Krathong", index: 24 },
-        { name: "Prove", index: 25 },
-        { name: "Soul", index: 26 },
-        { name: "Collect", index: 27 },
+        { name: "Beginning", index: 0, contest: "Pre-POSN1 DAY1" },
+        { name: "CIA Robbery", index: 1, contest: "Pre-POSN1 DAY1" },
+        { name: "Taiyaki", index: 2, contest: "Pre-POSN1 DAY1" },
+        { name: "Dream", index: 3, contest: "Pre-POSN1 DAY1" },
+        { name: "Poisoning", index: 4, contest: "Pre-POSN1 DAY1" },
+        { name: "Eliminate", index: 5, contest: "Pre-POSN1 DAY1" },
+        { name: "Prime Move On", index: 6, contest: "Pre-POSN1 DAY1" },
+        { name: "Croissant", index: 7, contest: "Pre-POSN1 DAY1" },
+        { name: "Trap", index: 8, contest: "Pre-POSN1 DAY1" },
+        { name: "Rope Puzzle", index: 9, contest: "Pre-POSN1 DAY1" },
+        { name: "EXTRA", index: 10, contest: "Pre-POSN1 DAY1" },
+        { name: "Rocket", index: 11, contest: "Pre-POSN1 DAY2" },
+        { name: "Pond", index: 12, contest: "Pre-POSN1 DAY2" },
+        { name: "4 Khon", index: 13, contest: "Pre-POSN1 DAY2" },
+        { name: "DevJ and palindrome", index: 14, contest: "Pre-POSN1 DAY2" },
+        { name: "UOI", index: 15, contest: "Pre-POSN1 DAY2" },
+        { name: "Hurricane", index: 16, contest: "Pre-POSN1 DAY2" },
+        { name: "Sakubomb", index: 17, contest: "Pre-POSN1 DAY2" },
+        { name: "sleepntjoop", index: 18, contest: "Pre-POSN1 DAY2" },
+        { name: "Seating", index: 19, contest: "Pre-POSN1 DAY2" },
+        { name: "Friend", index: 20, contest: "Pre-POSN1 DAY2" },
+        { name: "EXTRA", index: 21, contest: "Pre-POSN1 DAY2" },
+        { name: "Original Krathong", index: 22, contest: "Junior Dev Loy Krathong Contest 2024" },
+        { name: "super memory", index: 23, contest: "Junior Dev Loy Krathong Contest 2024" },
+        { name: "Baidu X LOY Krathong", index: 24, contest: "Junior Dev Loy Krathong Contest 2024" },
+        { name: "Prove", index: 25, contest: "Junior Dev Loy Krathong Contest 2024" },
+        { name: "Soul", index: 26, contest: "Junior Dev Loy Krathong Contest 2024" },
+        { name: "Collect", index: 27, contest: "Junior Dev Loy Krathong Contest 2024" },
     ];
 
     // Update type here to match the structure of the problemsList
     const [searchTerm, setSearchTerm] = useState<string>("");
-    const [filteredProblems, setFilteredProblems] = useState<{ name: string, index: number }[]>(problemsList);
+    const [filteredProblems, setFilteredProblems] = useState<{ name: string, index: number, contest: string }[]>(problemsList);
 
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         const term = event.target.value.toLowerCase();
         setSearchTerm(term);
         setFilteredProblems(
             problemsList.filter((problem) =>
-                problem.name.toLowerCase().includes(term) || problem.index.toString().includes(term)
+                problem.name.toLowerCase().includes(term) || problem.index.toString().includes(term) || problem.contest.toLowerCase().includes(term)
             )
         );
     };
@@ -76,9 +76,7 @@ export default function Problems() {
                 </div>
                 <div className="listheader">
                     <h1>Problem Name</h1>
-                    <h1>Level</h1>
                     <h1>Contest</h1>
-                    <h1>Progress</h1>
                 </div>
                 <div className="box">
                     <div className="list">
@@ -86,8 +84,11 @@ export default function Problems() {
                             filteredProblems.map((problem) => (
                                 <div className="problemlist" key={problem.index}>
                                     <a className="problem" href={`/problems/${problem.index}`}>
-                                        <div className="problemname">{problem.name}</div>
-                                        <div className="problemnum">{problem.index}</div>
+                                        <div className="titleproblemwow">
+                                            <div className="problemname">{problem.name}</div>
+                                            <div className="problemnum">{problem.index}</div>
+                                        </div>
+                                        <div className="problemcontest">{problem.contest}</div>
                                     </a>
                                 </div>
                             ))
