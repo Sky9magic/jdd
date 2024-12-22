1bb58d7cae735314c13dce7dfd170bc7df79d311
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-23 00:29:00 +0700

    asdf

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,6 +5,21 @@ import Link from "next/link";
 import "../styles/footer_styles.css";
 
 export default function Footer() {
+    const [theme, setTheme] = useState(() => {
+        if (typeof window !== "undefined") {
+            return document.body.getAttribute("data-theme") || "light";
+        }
+        return "light"; 
+    });
+
+    useEffect(() => {
+        document.body.setAttribute("data-theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+
     return (
         <footer className="Footer">
             <center className="text-footer">
