7a31d8b5148b2394f69f4f9f36eadf6d6e4a18b5
NagornPh <167499794+NagornPh@users.noreply.github.com>
2024-12-23 00:34:39 +0700

    adf

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,32 @@ import Link from "next/link";
 import "../styles/footer_styles.css";
 
 export default function Footer() {
+    const [theme, setTheme] = useState(() => {
+        if (typeof window !== "undefined") {
+            return document.body.getAttribute("data-theme") || "light";
+        }
+        return "light"; 
+    });
+
+    useEffect(() => {
+        document.body.setAttribute("data-theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+
     return (
         <footer className="Footer">
             <center className="text-footer">
                 <Link href="/">© 2024 Junior Dev. All rights reserved.</Link>
             </center>
+            <div className="themebutton">
+                {/* Toggle Theme:  */}
+                <button className="theme-toggle-btn" onClick={toggleTheme}>
+                    {/* {theme === "light" ? "   🌙 Dark Mode   " : "   ☀️ Light Mode   "} */}
+                </button>
+            </div>
         </footer>
     );
 }
