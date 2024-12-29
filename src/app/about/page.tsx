200f5d3cae7f783f09afca1b1642bd147106b21b
Skymagic <Sky9magic@gmail.com>
2024-12-29 20:17:36 +0700

    lkjhgfd

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,17 +17,19 @@ export default function About() {
                     <h1 className="typing2">About</h1>
                 </div>
             </div>
-            <div className="main" style={{alignItems:"center",textAlign:"center"}}>
+            <div className="main" style={{alignItems:"center"}}>
                 <div className="desc">
-                    <div className="aurora-container">
-                        <p className="aurora-text">Junior dev is an organization for hosting contests on various topics for people to practice doing problem ranging from basic coding to IOI</p>
+                    <div style={{textAlign:"center"}}>
+                        <div className="aurora-container">
+                            <p className="aurora-text">Junior dev is an organization for hosting contests on various topics for people to practice doing problem ranging from basic coding to IOI</p>
+                        </div>
+                        <p>made with ❤ by junior dev team members and friends</p>
+                        <p>those who contributed to this project:</p>
                     </div>
-                    <p>made with ❤ by junior dev team members and friends</p>
-                    <p>those who contributed to this project:</p>
                     <div className="listww">
-                        <p className="person">1.kaopj</p>
-                        <p className="person">2.nagorn</p>
-                        <p className="person">3.uhhhh</p>
+                        <p className="person">1.</p>
+                        <p className="person">2.</p>
+                        <p className="person">3.</p>
                     </div>
                 </div>
             </div>
