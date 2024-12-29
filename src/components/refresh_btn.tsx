c25755414afb34ae760d453e6686d0efd179a3ad
Skymagic <Sky9magic@gmail.com>
2024-12-29 11:49:11 +0700

    Update refresh_btn.tsx

--- src/components/refresh_btn.tsx
+++ src/components/refresh_btn.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function RefreshButton() {
     return (
-        <button onClick={window.location.reload();}>
+        <button onClick={window.location.reload}>
             (If the page looks buggy, please refresh)
         </button>
     );
